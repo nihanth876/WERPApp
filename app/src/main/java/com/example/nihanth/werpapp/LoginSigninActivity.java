fb8a1cf583978f968f3d4d407087a5786d93c94a
nihanth <nihanth876@gmail.com>
2019-02-18 19:49:58 +0530

    Commit

---
+++ app/src/main/java/com/example/nihanth/werpapp/LoginSigninActivity.java
@@ -0,0 +1,16 @@
+package com.example.nihanth.werpapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class LoginSigninActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signin_layout); //this is temporary layout and will be changed when
+                                                //the layout is finalized
+
+    }
+}
