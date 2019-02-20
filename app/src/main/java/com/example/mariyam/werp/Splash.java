2859db40a498abf7c9201c3d3f3fe5d63c16ba0e
nihanth <nihanth876@gmail.com>
2019-02-20 21:20:06 +0530

    Commit

--- app/src/main/java/com/example/mariyam/werp/Splash.java
+++ app/src/main/java/com/example/mariyam/werp/Splash.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class Splash extends AppCompatActivity {
     private static int Splash = 4000;
@@ -11,7 +13,12 @@ public class Splash extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+
+
+
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
