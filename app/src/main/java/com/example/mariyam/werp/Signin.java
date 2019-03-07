b21ebbbc4f72ffefecaa2d777360fe3f066d3c00
nihanth <nihanth876@gmail.com>
2019-03-07 21:22:07 +0530

    Google Signin Added

--- app/src/main/java/com/example/mariyam/werp/Signin.java
+++ app/src/main/java/com/example/mariyam/werp/Signin.java
@@ -4,29 +4,41 @@ import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class Signin extends AppCompatActivity {
     private EditText loginemail,loginpassword,inputnewpassword;
     private Button resetpassword,newpassword;
-    private Button login;
+    private Button login,google;
     private FirebaseUser mUser;
     private FirebaseAuth.AuthStateListener mAuthListener;
     private ProgressDialog mProgressDialog;
     private FirebaseAuth mAuth;
+    //GoogleSignIn mGooglesignInClient;//added
+    private int RC_SIGN_IN=1;//added
+    GoogleSignInClient mGoogleSignInClient; //added
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +52,26 @@ public class Signin extends AppCompatActivity {
         login=(Button)findViewById(R.id.btnLogin);
         mProgressDialog=new ProgressDialog(this);
         mAuth=FirebaseAuth.getInstance();
+
+        //added
+
+        google = findViewById(R.id.btnSignupGoogle);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+        //till here
 
         resetpassword.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -185,6 +217,76 @@ public class Signin extends AppCompatActivity {
             this.finish();
         }
     }
+
+    //added
+    private void signIn() {
+        Log.d("hello","entered");
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Log.d("hello","iamhere");
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            Log.d("hello",task.isSuccessful()+"");
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                Log.d("hello","heretoo");
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w("hello", "Google sign in failed", e);
+                // ...
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d("hello", "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("hello", "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUImod(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("hello", "signInWithCredential:failure", task.getException());
+                            //Snackbar.make(findViewById(R.id.main_layout), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+                            updateUImod(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    private void updateUImod(FirebaseUser user) {
+
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+        if (acct != null) {
+            String personName = acct.getDisplayName();
+            String personGivenName = acct.getGivenName();
+            String personFamilyName = acct.getFamilyName();
+            String personEmail = acct.getEmail();
+            String personId = acct.getId();
+            Uri personPhoto = acct.getPhotoUrl();
+        }
+        Toast.makeText(this,"Logged In Successfully",Toast.LENGTH_SHORT).show();
+    }
+
+    //till here
 
 
 }
